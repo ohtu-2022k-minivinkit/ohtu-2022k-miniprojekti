ec3a3f4f04cad7f71185cc1ca37852e9b6e4e0c3
Matias Nisula <matiasnisula81@gmail.com>
2022-03-31 16:05:55 +0300

    database connection for sqlite

---
+++ src/database_connection.py
@@ -0,0 +1,10 @@
+from multiprocessing import connection
+import sqlite3
+
+connection = sqlite3.connect("database.sqlite")
+
+#Palauttaa sanakirjana
+connection.row_factory = sqlite3.Row
+
+def get_database_connection():
+    return connection

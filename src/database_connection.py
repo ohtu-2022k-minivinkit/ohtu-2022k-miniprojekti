aa0632507bc81e73dbd44b410200ea9fe9e05bb8
Matias Nisula <matiasnisula81@gmail.com>
2022-04-01 16:00:26 +0300

    fix pylint errors

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,10 +1,7 @@
 import sqlite3
 from config import DATABASE_FILE_PATH
 
-connection = sqlite3.connect(DATABASE_FILE_PATH)
-
-#Palauttaa sanakirjana
-connection.row_factory = sqlite3.Row
+CONNECTION = sqlite3.connect(DATABASE_FILE_PATH)
 
 def get_database_connection():
-    return connection
+    return CONNECTION

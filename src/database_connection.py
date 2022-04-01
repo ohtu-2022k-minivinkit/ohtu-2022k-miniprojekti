6738e3881abce109775582a89951e753cd05cd87
Matias Nisula <matiasnisula81@gmail.com>
2022-04-01 12:12:26 +0300

    use env variable DATABASE_FILE_PATH

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,6 +1,7 @@
 import sqlite3
+from config import DATABASE_FILE_PATH
 
-connection = sqlite3.connect("database.sqlite")
+connection = sqlite3.connect(DATABASE_FILE_PATH)
 
 #Palauttaa sanakirjana
 connection.row_factory = sqlite3.Row

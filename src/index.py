affd0b1ee2943618bf1e4e24d4f3977be549c1aa
Minna Ronka <minna.ronka@gmail.com>
2022-04-01 22:21:34 +0300

    Improve and fix code

--- src/index.py
+++ src/index.py
@@ -2,9 +2,9 @@ from ui.ui import UI
 from services.bookmark_service import bookmark_service
 
 def main():
-    # pylint: disable=invalid-name
-    ui = UI(bookmark_service)
-    ui.start()
+    # pylint: disable=invalid-name, kiitos, nyt lienee parempi
+    user_interface = UI(bookmark_service)
+    user_interface.start()
 
 if __name__ == '__main__':
     main()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,22 +1,19 @@
-# pylint: disable=invalid-name, no-self-use
-class ConsoleIO:
-    def write(self, text):
-        print(text)
-
-    def read(self, text):
-        text = input(text)
-        return text
+from ui.console_io import (
+    console_io as default_console_io
+)
+from entities.bookmark import Bookmark
 
 COMMANDS = {
     "x": "x lopeta",
     "1": "1 lisää vinkki",
     "2": "2 tulosta vinkit"
-}
+    }
 
 class UI:
-    def __init__(self, service, io=ConsoleIO()):
-        self._io = io
-        self._service = service
+    def __init__(self, service, input_output=default_console_io):
+        self._io = input_output
+        self._bookmark_service = service
+        self._error = False
 
     def start(self):
         self._io.write("")
@@ -24,6 +21,7 @@ class UI:
         self._print_info()
 
         while True:
+            self._error = False
             self._io.write("")
             command = self._io.read("komento: ")
             if not command in COMMANDS:
@@ -33,27 +31,45 @@ class UI:
 
             if command == "x":
                 break
+
             if command == "1":
                 self._add_bookmark()
-            elif command == "2":
-                self._list_bookmarks()
 
     def _print_info(self):
-        # pylint: disable=expression-not-assigned
-        [ self._io.write(command) for command in COMMANDS.values() ]
+        for command in COMMANDS.values():
+            self._io.write(command)
 
     def _add_bookmark(self):
         self._io.write("Lisätään uusi vinkki")
-        title = self._io.read("title: ")
-        link = self._io.read("link:")
 
-        print(title, link)
-        #tähän logiikan metodiin kutsu, esim. self._service._add_bookmark(title, link)?
-        #ja tarvitaan vielä myös virheviesti
+        title = self._io.read("otsikko: ")
+        self.check_title(title)
+        if self._error:
+            return
+
+        link = self._io.read("linkki: ")
+        self.check_link(link)
+        if self._error:
+            return
+
+        self._bookmark_service.create_bookmark(Bookmark(title, link))
+
+    def check_title(self, title):
+        title = title.strip()
+        # ehkä lisää ehtoja? nyt katsoo ettei title tyhjä tai yli 100 merkkiä
+        if not title or len(title) > 100:
+            self._error = True
+            self._io.write("otsikko puuttui tai oli liian pitkä (yli 100 merkkiä)")
+
+        # ? ehkä parempia ehtoja? nyt katsoo että alussa http ja väh 10 merkkiä pitkä
+    def check_link(self, link):
+        if len(link.strip()) < 10 and link[:4] != "http":
+            self._error = True
+            self._io.write("linkki oli virheellinen, anna otsikko ja linkki uudelleen")
 
     def _list_bookmarks(self):
-        bookmark_list = self._service.get_all_bookmarks()
-
+        bookmark_list = self._bookmark_service.get_all_bookmarks()
         if bookmark_list:
             for bookmark in bookmark_list:
-                print(f'{bookmark}\n')
+                self._io.write(f'{bookmark}\n')
+ 

19fddf6cb7fff6e3ceb44613870c64f421c9de35
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-04-02 07:58:14 +0300

    Remove placeholder_test.py

--- src/tests/placeholder_test.py
+++
@@ -1,6 +0,0 @@
-# Automaattista testausta varten. Voi poistaa heti kun oikeita yksikkötestejä.
-import unittest
-
-class Test(unittest.TestCase):
-    def test_placholder(self):
-        self.assertEqual(True, True)

f28e4da13c22d6b78489d154e12686e5b3f84166
Minna Ronka <minna.ronka@gmail.com>
2022-04-02 11:10:23 +0300

    Create bookmark

--- src/services/bookmark_service.py
+++ src/services/bookmark_service.py
@@ -1,14 +1,15 @@
 from repositories.bookmark_repository import (
     bookmark_repository as default_bookmark_repository
 )
+from entities.bookmark import Bookmark
 
 
 class BookmarkService():
     def __init__(self, bookmark_repository=default_bookmark_repository):
         self._bookmark_repository = bookmark_repository
 
-    def create_bookmark(self, bookmark):
-        self._bookmark_repository.create(bookmark)
+    def create_bookmark(self, title, link):
+        self._bookmark_repository.create(Bookmark(title, link))
 
     def get_all_bookmarks(self):
         return self._bookmark_repository.get_all()

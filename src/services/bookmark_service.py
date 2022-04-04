5289cdc9daf049010bbb54be9d6d06cf6311fbc8
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-04-04 10:23:43 +0300

    Add docstrings to bookmark_service.py

--- src/services/bookmark_service.py
+++ src/services/bookmark_service.py
@@ -6,12 +6,32 @@ from entities.bookmark import Bookmark
 
 class BookmarkService():
     def __init__(self, bookmark_repository=default_bookmark_repository):
+        """Creates new BookmarkService object.
+
+        Args:
+            bookmark_repository (BookmarkRepository, optional):
+                Repository where the bookmarks are stored. Defaults to
+                default_bookmark_repository.
+        """
         self._bookmark_repository = bookmark_repository
 
     def create_bookmark(self, title, link):
+        """Creates a new bookmark and stores it into the repository.
+
+        Args:
+            title (string): Title of the bookmark.
+            link (string): Bookmarked link.
+        """
         self._bookmark_repository.create(Bookmark(title, link))
 
     def get_all_bookmarks(self):
+        """Returns all bookmarks stored in the repository.
+
+        Returns all bookmarks stored in the repository as a list of Bookmark objects.
+
+        Returns:
+            list: List of Bookmark objects.
+        """
         return self._bookmark_repository.get_all()
 
 

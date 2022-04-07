6612f8bcd016ac863b49baedf2116007601352af
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-07 08:30:04 +0300

    Add exception handling to get_url_title

--- src/services/network_request.py
+++ src/services/network_request.py
@@ -9,9 +9,15 @@ def get_url_title(url):
     Returns:
         string: Title of the webpage.
     """
-    html = requests.get(url).text
+    try:
+        html = requests.get(url).text
+    except requests.exceptions.RequestException:
+        return ""
 
     title_start = html.find('<title>') + 7
     title_end = html.find('</title>')
+
+    if title_start == -1 or title_end == -1:
+        return ""
 
     return html[title_start:title_end]
